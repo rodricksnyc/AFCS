7092431e521844ae94880c0d8dd45ab99baa0169
1MAC <1mac@1MACs-iMac.local>
2019-09-23 09:44:07 -0400

    slide will be the innovations page

--- innovations.php
+++ innovations.php
@@ -213,6 +213,86 @@
       </div>
     </div>
 
+    <div class="swiper-slide">
+      <figure class="slide-bgimg" style="background-image:url(../images5/nasa.jpg)">
+        <img src="images5/nasa.jpg" class="entity-img" />
+      </figure>
+      <div class="content">
+        <p class="title">SPACE <span style="color:#33ccff;">INNOVATIONS</span><br>GPS CONSTELLATIONS</p>
+        <span class="caption">Did you know that GPS is provided by the U.S. Air Force to the entire world free of charge? It’s made possible by satellites developed by Air Force civilian researchers. These satellites orbit the earth broadcasting radio signals that allow a form of triangulation. Beyond providing highly accurate positioning that enable the navigation devices in our cars and on our smart phones, GPS enables critical networks like cellular phones, ATM banking, and power grids, to name a few. It also provides crucial precision navigation and timing capabilities to our military forces around the world. The latest addition to this system was successfully launched from Cape Canaveral on May 15, 2013. This GPS IIF-4 spacecraft was added to the orbiting constellation of 64 satellites circling 11,000 nautical miles above Earth.</span>
+      </div>
+    </div>
+
+    <div class="swiper-slide">
+      <figure class="slide-bgimg" style="background-image:url(../images5/rockets-innovations.jpg)">
+        <img src="images5/rockets-innovations.jpg" class="entity-img" />
+      </figure>
+      <div class="content">
+        <p class="title">SPACE <span style="color:#33ccff;">INNOVATIONS</span><br>ROCKETS</p>
+        <span class="caption">The Air Force Office of Scientific Research and NASA successfully launched a nine-foot rocket 1,300 feet over the skies of Indiana using an environmentally friendly, safe propellant composed of Aluminum powder and water ice (ALICE). ALICE has the consistency of toothpaste when made and can be fit into molds and cooled to -30° C, 24 hours before flight. The concept has really taken off with researchers for several reasons. When optimized, ALICE could have a higher performance than conventional propellants; it can be used both for flight on Earth and in long-distance space missions; and it can theoretically be manufactured on distant planets – the moon or Mars, for example – instead of having to be transported to those distant locations.</span>
+      </div>
+    </div>
+
+    <div class="swiper-slide">
+      <figure class="slide-bgimg" style="background-image:url(../images5/space-weather.jpg)">
+        <img src="images5/rockets-innovations.jpg" class="entity-img" />
+      </figure>
+      <div class="content">
+        <p class="title">SPACE <span style="color:#33ccff;">INNOVATIONS</span><br>SPACE WEATHER</p>
+        <span class="caption">The Coriolis satellite that carries Air Force Research Lab’s (AFRL) Solar Mass Ejection Imager, or SMEI, has successfully detected and tracked 360 coronal mass ejections (CMEs) and shown the practicality of forecasting effects at Earth of these hazardous space weather phenomena. Since 2003, SMEI cameras have captured nearly full sky images of great sensitivity every 103 minutes; there is no other celestial record of this length, field of view, continuity, sensitivity and cadence. Since 2003, SMEI cameras have captured nearly full sky images of great sensitivity every 103 minutes; there is no other celestial record of this length, field of view, continuity, sensitivity and cadence.</span>
+      </div>
+    </div>
+
+    <div class="swiper-slide">
+      <figure class="slide-bgimg" style="background-image:url(../images5/gps.jpg)">
+        <img src="images5/gps.jpg" class="entity-img" />
+      </figure>
+      <div class="content">
+        <p class="title">SPACE <span style="color:#33ccff;">INNOVATIONS</span><br>SATELLITE TECHNOLOGY</p>
+        <span class="caption">U.S. Government data received from satellites have increased 100,000-fold in the last decade due primarily to the addition of nearly 50 new instruments to the nation’s exploratory toolbox. Currently, scientists are studying information that satellites are gathering on how our solar system affects the weather. In addition to heating the land, the sun’s energy can create weather disturbances and drive ocean currents. Researchers monitoring the amounts of solar energy reaching the earth theorize that it is small increases in the sun's total energy output over the last century that’s responsible for climate change.</span>
+      </div>
+    </div>
+
+    <div class="swiper-slide">
+      <figure class="slide-bgimg" style="background-image:url(../images5/cyber-attack.jpg)">
+        <img src="images5/cyber-attack.jpg" class="entity-img" />
+      </figure>
+      <div class="content">
+        <p class="title">CYBER <span style="color:#33ccff;">INNOVATIONS</span><br>CYBER ATTACK</p>
+        <span class="caption">The Air Force Research Laboratory (AFRL) developed a Network Intrusion, Risk and Vulnerability Analysis (NIRVANA) software tool to provide comprehensive cyber security analysis capability. The tool uses data from multiple intrusion detection systems to produce a comprehensive situation awareness picture, including a static evaluation of network vulnerabilities. NIRVANA identifies the consequences of actions. With this tool, network vulnerabilities are identified using the novel attack graphs associated with applications and mission requirements. The technology can estimate the probable targets intended by the attacker, allowing operators to counter with near-real-time actions, effectively denying the attacker’s goals. The technique is designed to scale, allowing for coordination of multiple security domains, and several runtime operators.</span>
+      </div>
+    </div>
+
+    <div class="swiper-slide">
+      <figure class="slide-bgimg" style="background-image:url(../images5/nano.jpg)">
+        <img src="images5/nano.jpg" class="entity-img" />
+      </figure>
+      <div class="content">
+        <p class="title">CYBER <span style="color:#33ccff;">INNOVATIONS</span><br>NANO TECHNOLOGY</p>
+        <span class="caption">While nanoscientists study matter on an atomic and molecular scale, nanotechnologists develop materials and devices 100 nanometers or smaller in one or more dimensions. (A nanometer is one billionth of a meter, or the comparative size of a marble to the earth; a meter is about three feet.) Since it began in the 1980s, their work has advanced on two tracks – controlling self-recognizable/self-assembling molecular components and scaling down larger entities without atomic-level control. The seemingly infinite number of potential applications will impact multiple fields: robotics, medicine, electronics, and geo-engineering are just a few. AFCS is pushing the leading edge of research and development in this fascinating field.</span>
+      </div>
+    </div>
+
+    <div class="swiper-slide">
+      <figure class="slide-bgimg" style="background-image:url(../images5/router.jpg)">
+        <img src="images5/router.jpg" class="entity-img" />
+      </figure>
+      <div class="content">
+        <p class="title">CYBER <span style="color:#33ccff;">INNOVATIONS</span><br>WORLD'S FASTEST ROUTER</p>
+        <span class="caption">The U.S. Air Force recently completed testing on a new “flying wireless router” system that will help give ground forces and pilots real-time tactical information in the field. The main difference between this router and a typical one you might be using at home is that it is attached to fighter planes. It is essentially a software upgrade called Net-T, for “network tactical,” and is designed to facilitate almost instantaneous communication between commanders, ground troops, and fighter aircraft. This new development provides real-time information like videos, images, maps, coordinates, or any file type from the forward deployed elements without relying on satellite, radio or other forms of traditional communication</span>
+      </div>
+    </div>
+
+    <div class="swiper-slide">
+      <figure class="slide-bgimg" style="background-image:url(../images5/super.jpg)">
+        <img src="images5/super.jpg" class="entity-img" />
+      </figure>
+      <div class="content">
+        <p class="title">CYBER <span style="color:#33ccff;">INNOVATIONS</span><br>SUPER COMPUTING</p>
+        <span class="caption">The game console’s price-to-performance ratio inspired one AFCS research team to place an order for 1,700 of them to go with the 336 they already have. The system will use 300 to 320 kilowatts at full bore and about 10 percent to 30 percent of that in standby, when most supercomputers are using 5 megawatts. However, much of the time the cluster will only be running the nodes it needs and it will be turned off when not in use. By taking advantage of a growing gaming market, the price performance is just $2 to $3 per gigaFLOPS. The research team has been able to take advantage of about 60 percent of the PlayStations’ performance ability. Once complete, they are expecting to have a unit capable of 500 teraFLOPS or 500 trillion operations per second.</span>
+      </div>
+    </div>
+
   </div>
   <!-- If we need navigation buttons -->
   <div class="swiper-button-prev swiper-button-white"></div>
@@ -271,6 +351,78 @@
         <p class="title">RPAs</p>
       </div>
     </div>
+    <div class="swiper-slide">
+      <figure class="slide-bgimg" style="background-image:url(../images5/nasa.jpg)">
+        <img src="images5/nasa.jpg" class="entity-img" />
+      </figure>
+      <div class="content">
+        <p class="title">GPS CONSTELLATIONS</p>
+      </div>
+    </div>
+    <div class="swiper-slide">
+      <figure class="slide-bgimg" style="background-image:url(../images5/rockets-innovations.jpg)">
+        <img src="images5/rockets-innovations.jpg" class="entity-img" />
+      </figure>
+      <div class="content">
+        <p class="title">ROCKETS</p>
+      </div>
+    </div>
+
+    <div class="swiper-slide">
+      <figure class="slide-bgimg" style="background-image:url(../images5/space-weather.jpg)">
+        <img src="images5/space-weather.jpg" class="entity-img" />
+      </figure>
+      <div class="content">
+        <p class="title">SPACE WEATHER</p>
+      </div>
+    </div>
+
+    <div class="swiper-slide">
+      <figure class="slide-bgimg" style="background-image:url(../images5/gps.jpg)">
+        <img src="images5/gps.jpg" class="entity-img" />
+      </figure>
+      <div class="content">
+        <p class="title">SATELLITE TECHNOLOGY</p>
+      </div>
+    </div>
+
+    <div class="swiper-slide">
+      <figure class="slide-bgimg" style="background-image:url(../images5/cyber-attack.jpg)">
+        <img src="images5/cyber-attack.jpg" class="entity-img" />
+      </figure>
+      <div class="content">
+        <p class="title">CYBER ATTACK</p>
+      </div>
+    </div>
+
+    <div class="swiper-slide">
+      <figure class="slide-bgimg" style="background-image:url(../images5/nano.jpg)">
+        <img src="images5/nano.jpg" class="entity-img" />
+      </figure>
+      <div class="content">
+        <p class="title">NANO TECHNOLOGY</p>
+      </div>
+    </div>
+
+    <div class="swiper-slide">
+      <figure class="slide-bgimg" style="background-image:url(../images5/router.jpg)">
+        <img src="images5/nano.jpg" class="entity-img" />
+      </figure>
+      <div class="content">
+        <p class="title">WORLD'S FASTEST ROUTER</p>
+      </div>
+    </div>
+
+    <div class="swiper-slide">
+      <figure class="slide-bgimg" style="background-image:url(../images5/super.jpg)">
+        <img src="images5/nano.jpg" class="entity-img" />
+      </figure>
+      <div class="content">
+        <p class="title">SUPER COMPUTING</p>
+      </div>
+    </div>
+
+
   </div>
 </div>
 
