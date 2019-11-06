8a943028753009a35003d5eba9d0d69f9ddc5819
rodricksnyc <rodricksnyc@gmail.com>
2019-11-05 19:14:02 -0500

    fd

---
+++ css5/nav-include/navbar.php
@@ -0,0 +1,147 @@
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+  <div class="container-fluid hideNav">
+    <div class="collapse navbar-collapse navbar-right two">
+      <ul class="nav navbar-nav navbar-right">
+        <li class="searchIt">
+         <a class="grayBlock" href="#">
+           <i class="fas fa-search"></i>
+          </a>
+        </li>
+          <li><span id="thisSlided" class="bubble noShow">
+            <form class="navbar-form navbar-right" role="search">
+              <div class="horizontal">
+                <div id="barTop" class="con hideAgain2">
+                  <div class="bar top"></div>
+                  <div class="bar middle"></div>
+                  <div class="bar bottom"></div>
+                </div>
+                <div class="form-group">
+                  <div class="tipue_search_group">
+                    <div class="horizontalDiv">
+                  <input type="text" name="q" id="tipue_search_input" pattern=".{3,}" title="At least 3 characters" required>
+                    </input>
+
+                  </div>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </span>
+        </li>
+        <li>
+          <a class="navyBlock" href="roa.html">Register</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="afcs-homepage.php">
+        <img class="img-responsive navLogo" src="images5/afcs-navbar.png">
+      </a>
+    </div>
+    <div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
+
+      <span id="box2">
+        <ul class="nav navbar-nav makeToggle tabs" id="menu2">
+          <li>
+            <a id="list1" class="highlight one thing" href="afcs-homepage.php">Home</a>
+          </li>
+          <li>
+            <a id="list2" class="highlight two thing" href="about.php">About</a>
+          </li>
+          <li>
+            <a id="list3" class="active fun darker highlight three" href="careers.php">Careers</a>
+          </li>
+          <li id="list4">
+            <a class="highlight four thing" href="find-a-job.php">Find A Job</a>
+          </li>
+          <li>
+            <a id="list5" class="highlight five thing" href="contact.php">Contact</a>
+          </li>
+
+        </span>
+        <span class='nope'>
+          <div class="spreadAcross">
+            <li>
+              <a href="https://www.facebook.com/AirForceCivilianService/" target="_blank" alt="AFCS Facebook page" class="social-head">
+                <i class="fab fa-facebook"></i>
+              </a>
+            </li>
+            <li>
+              <a class="social-head" href="https://www.linkedin.com/company/air-force-civilian-service" target="_blank" alt="AFCS LinkedIn page">
+                <i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
+            </li>
+            <li>
+              <a class="social-head" href="https://www.instagram.com/afcivilian/" target="_blank" alt="AFCS Instagram">
+                <i class="fab fa-instagram"></i>
+              </a>
+            </li>
+            <li>
+              <a class="social-head" href="https://twitter.com/afcivilian/" target="_blank" alt="AFCS Twitter">
+                <i class="fa fa-twitter-square" aria-hidden="true"></i>
+              </a>
+            </li>
+            <li>
+              <a class="social-head" href="https://www.youtube.com/channel/UC1L_oa0A2iMfJFuNa44EcFQ" target="_blank" alt="AFCS YouTube link">
+                <i class="fa fa-youtube-square" aria-hidden="true"></i>
+              </a>
+            </li>
+          </div>
+
+          <ul class="nav navbar-nav makeToggle tabs">
+            <li class="allAcross">
+              <a id="liSearch" class="highlight three thing" href="#">Search</a>
+            </li>
+
+            <li class="allAcross">
+              <a id="liRegister" class="highlight three thing" href="roa.php">Register</a>
+            </li>
+          </ul>
+        </span>
+
+        <li class="social">
+          <a id="liSocial" class="blacker" href="#">Social</a>
+        </li>
+      </ul>
+
+      <span id="funStuff" class="icon noShow2">
+        <ul class="nav navbar-nav">
+          <li style="list-style-type:none !important">
+            <a href="#">
+              <div class="con hideAgain">
+                <div class="bar top"></div>
+                <div class="bar middle"></div>
+                <div class="bar bottom"></div>
+              </div>
+            </a>
+          </li>
+          <li class="blackLine" style="list-style-type:none !important"></li>
+          <li style="list-style-type:none !important">
+            <a href="https://www.facebook.com/AirForceCivilianService/" target="_blank" alt="AFCS Facebook page" class="social-head"><i class="fab fa-facebook"></i>
+            </a>
+          </li>
+          <li style="list-style-type:none !important">
+            <a class="social-head" href="https://www.linkedin.com/company/air-force-civilian-service" target="_blank" alt="AFCS LinkedIn page"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
+          </li>
+          <li style="list-style-type:none !important">
+            <a class="social-head" href="https://www.instagram.com/afcivilian/" target="_blank" alt="AFCS Instagram"><i class="fab fa-instagram"></i></a>
+          </li>
+          <li style="list-style-type:none !important">
+            <a class="social-head" href="https://twitter.com/afcivilian/" target="_blank" alt="AFCS Twitter"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
+          </li>
+          <li style="list-style-type:none !important">
+            <a class="social-head" href="https://www.youtube.com/channel/UC1L_oa0A2iMfJFuNa44EcFQ" target="_blank" alt="AFCS YouTube link"><i class="fa fa-youtube-square" aria-hidden="true"></i></a>
+          </li>
+        </ul>
+      </span>
+    </div>
+  </div>
+
+</nav>
